9a350abb534b6ed9d84d56f3f8d5e19ff79a34de
Paul Geraghty <muse@appsthatcould.be>
2014-08-16 21:51:20 +0200

    Add installation of different FFmpeg versions to Travis CI config

---
+++ .travis/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+case "$FFMPEG_VERSION" in
+    2.3)
+        sudo add-apt-repository ppa:archivematica/externals -y
+        sudo apt-get update -q
+        sudo apt-get install ffmpeg
+        ;;
+
+    1.2)
+        stop
+        ;;
+    *)
+        sudo apt-get update -q
+        sudo apt-get install ffmpeg
+esac
