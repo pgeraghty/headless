674509089b186ebab6f16e969478fcf1128a8dac
Paul Geraghty <muse@appsthatcould.be>
2014-08-18 01:54:16 +0200

    Handle duration format differences

--- spec/features/capture_video_spec.rb
+++ spec/features/capture_video_spec.rb
@@ -9,8 +9,15 @@ describe Headless::VideoRecorder do
 
     sleep 5 # give Xvfb some time
     `DISPLAY=:99 fluxbox > /dev/null 2>&1 &`
+    sleep 3
     `ffmpeg -f x11grab -r 30 -s 1280x720 -i :99.0 -vcodec mpeg4 -qscale:v 2 -threads 1 -y -t 10 #{capture_file} > #{log} 2>&1`
-    expect(File.read log).to match(/time=00:00:10/)
+    expected_duration =
+        if h.video.guess_ffmpeg_version! > Gem::Version.new(1.2)
+          /time=00:00:10/
+        else
+          /time=10/
+        end
+    expect(File.read log).to match(expected_duration)
     expect(`ffprobe #{capture_file} 2>&1 | grep Duration`).to match(/Duration: 00:00:10/)
     `rm #{capture_file}`
     expect { h.destroy }.not_to raise_error
