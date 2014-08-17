471a21da22080c0e1d8556a5cf61cc68b6f69789
Paul Geraghty <muse@appsthatcould.be>
2014-08-18 01:32:24 +0200

    Replace Process.spawn, not available in 1.8.7

--- spec/features/capture_video_spec.rb
+++ spec/features/capture_video_spec.rb
@@ -8,13 +8,11 @@ describe Headless::VideoRecorder do
     log = '/tmp/ffmpeg_test.log'
 
     sleep 5 # give Xvfb some time
-    fluxbox = Process.spawn("DISPLAY=:99} fluxbox", [:out, :err] => '/dev/null')
+    `DISPLAY=:99 fluxbox & > /dev/null 2>&1 &`
     `ffmpeg -f x11grab -r 30 -s 1280x720 -i :99.0 -c:v mpeg4 -qscale:v 2 -threads 1 -y -t 10 #{capture_file} > #{log} 2>&1`
     expect(File.read log).to match(/time=00:00:10.00/)
     expect(`ffprobe #{capture_file} 2>&1 | grep Duration`).to match(/Duration: 00:00:10.00/)
     `rm #{capture_file}`
-    Process.detach fluxbox
     expect { h.destroy }.not_to raise_error
-    Process.kill(9, fluxbox) rescue nil
   end
 end if Headless::CliUtil.application_exists?('ffmpeg')
