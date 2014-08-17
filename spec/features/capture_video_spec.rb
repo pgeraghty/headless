708f8ef4066c16e619451a7baab8345f7cf23a37
Paul Geraghty <muse@appsthatcould.be>
2014-08-18 01:46:20 +0200

    Tolerate fractional duration difference in RSpec matcher

--- spec/features/capture_video_spec.rb
+++ spec/features/capture_video_spec.rb
@@ -10,8 +10,8 @@ describe Headless::VideoRecorder do
     sleep 5 # give Xvfb some time
     `DISPLAY=:99 fluxbox > /dev/null 2>&1 &`
     `ffmpeg -f x11grab -r 30 -s 1280x720 -i :99.0 -vcodec mpeg4 -qscale:v 2 -threads 1 -y -t 10 #{capture_file} > #{log} 2>&1`
-    expect(File.read log).to match(/time=00:00:10.00/)
-    expect(`ffprobe #{capture_file} 2>&1 | grep Duration`).to match(/Duration: 00:00:10.00/)
+    expect(File.read log).to match(/time=00:00:10/)
+    expect(`ffprobe #{capture_file} 2>&1 | grep Duration`).to match(/Duration: 00:00:10/)
     `rm #{capture_file}`
     expect { h.destroy }.not_to raise_error
   end
