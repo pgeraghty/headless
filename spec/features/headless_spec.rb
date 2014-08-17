6bd25160e63c57008aec9b3c4f35d70030a1b3da
Paul Geraghty <muse@appsthatcould.be>
2014-08-18 01:08:33 +0200

    Add experimental integration tests

---
+++ spec/features/headless_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Headless do
+  let(:h) { Headless.new }
+
+  it 'should start Xvfb when instantiated' do
+    expect(h).to be_instance_of(Headless)
+  end
+
+  it 'should destroy Xvfb when requested' do
+    expect { h.destroy }.not_to raise_error
+  end
+end
